cca7b7d8fba504401d3bf750187a475ae2dd3923
Yenchen <k307849@gmail.com>
2025-05-22 11:14:43 +0800

    normal

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-import createNextIntlPlugin from 'next-intl/plugin';
-
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
-
 const nextConfig = {
   // 其他 Next.js 配置
 };
-
-export default withNextIntl(nextConfig);
