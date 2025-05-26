5a5ea2a9cf78a6c406d9d7def05589dfad53353f
Yenchen <k307849@gmail.com>
2025-05-26 14:34:30 +0800

    ERROR PAGE

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -3,12 +3,14 @@
 import React from 'react';
 import { useTranslations } from 'next-intl';
 import { HeroSection, ShowcaseSection, TechStack } from '@/components/sections/index-page/index';
+import NotFound from './not-found';
 
 const HomePage = () => {
   const t = useTranslations('home');
 
   return (
     <div className="min-h-screen">
+      <NotFound />
       <HeroSection />
       <ShowcaseSection />
       <TechStack />
